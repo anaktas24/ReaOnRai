7d78f5f11e20ab36a5777f6da2a99d4087ae0c16
anaktas24 <johnantony24@hotmail.com>
2024-10-19 23:19:09 +0200

    added folders and files

--- Mango/frontend/src/App.tsx
+++ Mango/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import correctly
-import Navbar from './components/Navbar'
-import About from './pages/About';
-import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
+import About from './pages/About/About';
+import Home from './pages/Home/Home'
 
 const App: React.FC = () => {
   return (
-    <Router> {/* Use BrowserRouter to wrap the entire app */}
+    <Router>
       <Navbar />
       <Routes> 
         <Route path="/" element={<Home />} />
